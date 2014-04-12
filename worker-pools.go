984a7bd4f5830d7fec99b0d9f35aefe92026bb9e
Uwe Janssen <mail@uwe-janssen.de>
2014-04-12 10:45:09 +0200

    split main into functions

--- worker-pools.go
+++ worker-pools.go
@@ -12,6 +12,12 @@ type result struct {
 	result int
 }
 
+func createChannels(num int) (chan task, chan result) {
+	input := make(chan task, num)
+	output := make(chan result, num)
+	return input, output
+}
+
 func worker(id int, input <-chan task, output chan<- result) {
 	for t := range input {
 		fmt.Println("worker", id, "processing task", t)
@@ -20,22 +26,32 @@ func worker(id int, input <-chan task, output chan<- result) {
 	}
 }
 
-func main() {
-	tasks := 9
-	input := make(chan task, tasks)
-	output := make(chan result, tasks)
-
-	for w := 1; w <= 3; w++ {
+func createWorker(num int, input <-chan task, output chan<- result) {
+	for w := 1; w <= num; w++ {
 		go worker(w, input, output)
 	}
-	for id := 1; id <= tasks; id++ {
+}
+
+func createTasks(num int, input chan<- task) {
+	for id := 1; id <= num; id++ {
 		input <- task{id: id}
 	}
 	close(input)
+}
 
-	for a := 1; a <= tasks; a++ {
+func readResults(num int, output <-chan result) {
+	for a := 1; a <= num; a++ {
 		d := <-output
 		fmt.Println("task", d.task, "result", d.result)
 	}
-	close(output)
+}
+
+func main() {
+	tasks := 9
+	input, output := createChannels(tasks)
+
+	createTasks(tasks, input)
+	createWorker(3, input, output)
+
+	readResults(tasks, output)
 }
