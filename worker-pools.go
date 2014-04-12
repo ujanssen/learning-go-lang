e33f14a29315bfe05e967be126ecac7d891b0efd
Uwe Janssen <mail@uwe-janssen.de>
2014-04-12 10:22:08 +0200

    have a type result struct

--- worker-pools.go
+++ worker-pools.go
@@ -4,35 +4,38 @@ import "fmt"
 import "time"
 
 type task struct {
-	id     int
+	id int
+}
+
+type result struct {
+	task   task
 	result int
 }
 
-func worker(id int, input <-chan task, output chan<- task) {
+func worker(id int, input <-chan task, output chan<- result) {
 	for t := range input {
-		fmt.Println("worker", id, "processing task", t.id)
-		t.result = t.id * 2
+		fmt.Println("worker", id, "processing task", t)
 		time.Sleep(time.Second)
-		output <- t
+		output <- result{task: t, result: t.id * 2}
 	}
 }
 
 func main() {
 	tasks := 9
 	input := make(chan task, tasks)
-	output := make(chan task, tasks)
+	output := make(chan result, tasks)
 
 	for w := 1; w <= 3; w++ {
 		go worker(w, input, output)
 	}
-	for j := 1; j <= tasks; j++ {
-		input <- task{id: j}
+	for id := 1; id <= tasks; id++ {
+		input <- task{id: id}
 	}
 	close(input)
 
 	for a := 1; a <= tasks; a++ {
-		t := <-output
-		fmt.Println("task", t.id, "result", t.result)
+		d := <-output
+		fmt.Println("task", d.task, "result", d.result)
 	}
 	close(output)
 }
