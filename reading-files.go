6677cab4870e684ff22a7661204a873e03bd7c7f
Uwe Janssen <mail@uwe-janssen.de>
2014-04-13 09:38:32 +0200

    read json from file

--- reading-files.go
+++ reading-files.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 )
 
+type Config struct {
+	Worker int `json:"worker"`
+	Tasks  int `json:"tasks"`
+}
+
 func main() {
+	var conf Config
 	dat, err := ioutil.ReadFile("reading-files.conf")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("dat", dat)
+
+	if err = json.Unmarshal(dat, &conf); err != nil {
+		panic(err)
+	}
+	fmt.Println("conf", conf)
 }
