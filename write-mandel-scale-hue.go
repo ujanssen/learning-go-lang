e6f604e1e73084d47da5dd50d4b905f78b656653
Uwe Janssen <mail@uwe-janssen.de>
2014-04-19 17:58:32 +0200

    refactor new getColor func

--- write-mandel-scale-hue.go
+++ write-mandel-scale-hue.go
@@ -10,9 +10,18 @@ import (
 	"task"
 )
 
-func main() {
-	var err error
+func getColor(iter, colScale int) color.Color {
 	var c color.Color
+	var err error
+	h := float64(iter) / float64(colScale)
+	h = h - math.Floor(h)
+	if c, err = hue.Color(h); err != nil {
+		panic(err)
+	}
+	return c
+}
+
+func main() {
 	var maxIter = 10 * 360
 	var d = mandel.NewData(640, 400, -0.7435669, 0.1314023, 0.0022878)
 
@@ -25,12 +34,7 @@ func main() {
 
 		for t := range it {
 			iter := mandel.Iterate(t.CRe, t.CIm, maxIter)
-			h := float64(iter) / float64(colScale)
-			h = h - math.Floor(h)
-			if c, err = hue.Color(h); err != nil {
-				panic(err)
-			}
-			pi.Img.Set(t.X, d.MaxY-t.Y, c)
+			pi.Img.Set(t.X, d.MaxY-t.Y, getColor(iter, colScale))
 		}
 		pi.Save()
 	}
