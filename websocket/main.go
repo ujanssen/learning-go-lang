503769ecae7ce29c5bbef79aa1901988cc42e936
Uwe Janssen <mail@uwe-janssen.de>
2014-06-10 23:34:04 +0200

    send message from go server to js client

--- websocket/main.go
+++ websocket/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 
 	"code.google.com/p/go.net/websocket"
 )
+
+type Message struct {
+	Head string `json:"head"`
+	Body string `json:"body"`
+}
+
+func (self *Message) String() string {
+	return self.Head + " with " + self.Body
+}
+
+func receive(ws *websocket.Conn) {
+	log.Println("Listening receiving from client")
+	var msg Message
+	err := websocket.JSON.Receive(ws, &msg)
+	if err == io.EOF {
+		log.Println("Receive io.EOF:", err)
+	} else if err != nil {
+		log.Println("Receive Error:", err, msg)
+	} else {
+		log.Println("Received: ", msg)
+	}
+}
 
 func listen() {
 	log.Println("Listening ws server on /websocket ...")
@@ -15,16 +38,20 @@ func listen() {
 		defer func() {
 			err := ws.Close()
 			if err != nil {
-				log.Println("Error:", err)
+				log.Println("Close Error:", err)
 			}
 		}()
 		log.Println("Created onConnected handler")
 		log.Println("onConnected: ", ws)
+		go receive(ws)
+
+		websocket.JSON.Send(ws, Message{"nead", "body"})
 
 	}
 	http.Handle("/websocket", websocket.Handler(onConnected))
 
 }
+
 func main() {
 
 	// websocket server
@@ -34,6 +61,5 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("static")))
 
 	log.Println("Listening web server on :8044 ...")
-
 	log.Fatal(http.ListenAndServe(":8044", nil))
 }
--- websocket/static/websocket.js
+++ websocket/static/websocket.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 	var ws = new WebSocket("ws://localhost:8044/websocket");
+
+	ws.onmessage = function(e) {
+		alert(e.data);
+	};
+/*
+	ws.send($.toJSON({"head" : "greeting", "body": "hello"}));
+	ws.send({"head" : "greeting", "body": "hello"});
+	ws.send("hello");
+*/	
 });
