7b0f4d2f8e419252efc737a348db8531fabcb447
Uwe Janssen <mail@uwe-janssen.de>
2014-06-11 21:05:59 +0200

    have a handler for every method

--- websocket/static/websocket.js
+++ websocket/static/websocket.js
@@ -2,18 +2,21 @@ $(document).ready(function() {
 	var ws = new WebSocket("ws://localhost:8044/websocket");
 
 	ws.onmessage = function(e) {
+		$("#onmessage").append($("<li>").text(JSON.stringify(e)));
+
 		var msg = $.parseJSON(e.data);
-
-		$("#list").append($("<li>").text(msg.head));
-		$("#list").append($("<li>").text(msg.body));
-
 		msg.body = "server"
 		console.log("send: " + JSON.stringify(msg))
 		ws.send(JSON.stringify(msg));
 	};
-/*
-	ws.send($.toJSON({"head" : "greeting", "body": "hello"}));
-	ws.send({"head" : "greeting", "body": "hello"});
-	ws.send("hello");
-*/	
+
+	ws.onopen = function(e) {
+		$("#onopen").append($("<li>").text(JSON.stringify(e)));
+	};
+	ws.onerror = function(e) {
+		$("#onerror").append($("<li>").text(JSON.stringify(e)));
+	};
+	ws.onclose = function(e) {
+		$("#onclose").append($("<li>").text(JSON.stringify(e)));
+	};
 });
