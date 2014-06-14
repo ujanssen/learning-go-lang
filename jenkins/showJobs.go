89e0c5ccd8927c3cf2b73510ec739a61b81a4e4f
Uwe Janssen <mail@uwe-janssen.de>
2014-06-14 09:32:47 +0200

    parse jenkins response

--- jenkins/showJobs.go
+++ jenkins/showJobs.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
+
+type JobState struct {
+	Name  string `json:"name"`
+	URL   string `json:"url"`
+	Color string `json:"color"`
+}
+
+type Jenkins struct {
+	NodeDescription string     `json:"nodeDescription"`
+	Jobs            []JobState `json:"jobs"`
+}
 
 func main() {
 	jenkins := flag.String("jenkins", "127.0.0.1:8080", "Jenkins hostname")
@@ -24,5 +36,15 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Printf("%s\n", string(contents))
+
+		var data Jenkins
+
+		err = json.Unmarshal(contents, &data)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%v", data.Jobs)
 	}
 }
