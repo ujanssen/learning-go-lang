a4913a636cafc00576f5f6f2bed00dfec67ae9c2
Uwe Janssen <mail@uwe-janssen.de>
2014-06-18 07:22:43 +0200

    cleanup

--- websocket/clients.go
+++ websocket/clients.go
@@ -230,47 +230,6 @@ func (s *Server) Listen() {
 	}
 }
 
-// func listen() {
-// 	log.Println("Listening ws server on /websocket ...")
-
-// 	// websocket handler
-// 	onConnected := func(ws *websocket.Conn) {
-// 		defer func() {
-// 			err := ws.Close()
-// 			if err != nil {
-// 				log.Println("Close Error:", err)
-// 			}
-// 		}()
-// 		log.Println("Created onConnected handler")
-// 		log.Printf("onConnected ws: %v\n", ws)
-
-// 		//		websocket.JSON.Send(ws, Message{"hello", "client"})
-// 		log.Println("Listening receiving from client")
-// 		for {
-// 			select {
-// 			default:
-
-// 				var msg Message
-// 				err := websocket.JSON.Receive(ws, &msg)
-// 				if err == io.EOF {
-// 					log.Println("Connection closed:", err)
-// 					return
-// 				} else if err != nil {
-// 					log.Println("Receive Error:", err)
-// 				} else {
-// 					log.Println("Received: ", msg.String())
-// 					if msg.Head == "ping" {
-// 						websocket.JSON.Send(ws, Message{"pong", "pong"})
-// 					}
-// 				}
-// 			}
-// 		}
-
-// 	}
-// 	http.Handle("/websocket", websocket.Handler(onConnected))
-
-// }
-
 func main() {
 
 	// websocket server
