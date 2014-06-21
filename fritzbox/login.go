91a2500c8923a2b4762717fbf51b776e17160ba3
Uwe Janssen <mail@uwe-janssen.de>
2014-06-21 12:49:40 +0200

    decode xml

--- fritzbox/login.go
+++ fritzbox/login.go
@@ -3,11 +3,19 @@ package main
 //curl  http://192.168.12.1/login_sid.lua
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
+
+// SessionInfo has SID
+type SessionInfo struct {
+	SID       string `xml:"SID"`
+	Challenge string `xml:"Challenge"`
+	BlockTime string `xml:"BlockTime"`
+}
 
 func main() {
 	response, err := http.Get("http://fritz/login_sid.lua")
@@ -22,5 +30,10 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Printf("%s\n", string(contents))
+
+		var s SessionInfo
+		err = xml.Unmarshal(contents, &s)
+
+		fmt.Printf("SessionInfo: %s\n", s)
 	}
 }
