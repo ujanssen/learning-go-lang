3b82ca4dde0c9abce61e24906499906dfd82a28c
Uwe Janssen <mail@uwe-janssen.de>
2014-07-06 08:15:16 +0200

    make consumer robust

--- concurrency/state_consumer.go
+++ concurrency/state_consumer.go
@@ -2,26 +2,41 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
-	"fmt"
 	"github.com/ujanssen/learning-go-lang/concurrency/state"
 	"log"
+	"time"
 )
 
 var origin = "http://localhost/"
 var url = "ws://localhost" + state.Port + state.Path
 
 func main() {
-	ws, err := websocket.Dial(url, "", origin)
-	if err != nil {
-		log.Fatal(err)
+	for {
+		ws := dial()
+		consume(ws)
 	}
+}
 
+func dial() *websocket.Conn {
+	for {
+		ws, err := websocket.Dial(url, "", origin)
+		if err == nil {
+			return ws
+		} else {
+			log.Println(err)
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func consume(ws *websocket.Conn) (err error) {
 	var s state.Message
 	for {
-		err := websocket.JSON.Receive(ws, &s)
+		err = websocket.JSON.Receive(ws, &s)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			return
 		}
-		fmt.Printf("Receive: %s\n", s)
+		log.Printf("Receive: %s\n", s)
 	}
 }
