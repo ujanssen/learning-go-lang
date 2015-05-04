9202a9b20538d7d467647d8483d30d8bfabc2b6b
Uwe Janssen <mail@uwe-janssen.de>
2015-05-04 20:37:51 +0200

    add functions

--- xapi/xapi.go
+++ xapi/xapi.go
@@ -23,6 +23,61 @@ type Session struct {
 	Originator       string
 }
 
+func (Session *Session) logout_subject_identifier() {
+}
+func (Session *Session) get_all_subject_identifiers() {
+}
+func (Session *Session) local_logout() {
+}
+func (Session *Session) slave_local_login_with_password() {
+}
+func (Session *Session) change_password() {
+}
+func (Session *Session) logout() {
+}
+func (Session *Session) login_with_password() {
+}
+func (Session *Session) remove_from_other_config() {
+}
+func (Session *Session) add_to_other_config() {
+}
+func (Session *Session) set_other_config() {
+}
+func (Session *Session) get_originator() {
+}
+func (Session *Session) get_parent() {
+}
+func (Session *Session) get_tasks() {
+}
+func (Session *Session) get_rbac_permissions() {
+}
+func (Session *Session) get_auth_user_name() {
+}
+func (Session *Session) get_auth_user_sid() {
+}
+func (Session *Session) get_validation_time() {
+}
+func (Session *Session) get_subject() {
+}
+func (Session *Session) get_is_local_superuser() {
+}
+func (Session *Session) get_other_config() {
+}
+func (Session *Session) get_pool() {
+}
+func (Session *Session) get_last_active() {
+}
+func (Session *Session) get_this_user() {
+}
+func (Session *Session) get_this_host() {
+}
+func (Session *Session) get_uuid() {
+}
+func (Session *Session) get_by_uuid() {
+}
+func (Session *Session) get_record() {
+}
+
 func main() {
 	fmt.Println("it compiles")
 }
