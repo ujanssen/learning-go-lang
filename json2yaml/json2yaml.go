8e66ef664ee12f079c724f28acd6fa523bfcaa0d
Uwe Janssen <mail@uwe-janssen.de>
2015-09-14 11:14:26 +0200

    add error

--- json2yaml/json2yaml.go
+++ json2yaml/json2yaml.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -8,28 +9,45 @@ import (
 	"github.com/ghodss/yaml"
 )
 
-func stdin2yaml() (s string) {
+var errEmpty = errors.New("stdin is empty")
+
+// read json from stdin
+// print as yaml
+func main() {
+	if err := checkStdin(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(readStdinConvertToYaml())
+}
+
+func checkStdin() (noError error) {
+	fi, err := os.Stdin.Stat()
+
+	if err != nil {
+		return err
+	}
+	if fi.Size() == 0 {
+		return errEmpty
+	}
+
+	return noError
+}
+
+func readStdinConvertToYaml() (s string) {
 	if json, err := ioutil.ReadAll(os.Stdin); err != nil {
 		s = err.Error()
 	} else {
-		if y, err := yaml.JSONToYAML(json); err != nil {
-			s = err.Error()
-		} else {
-			s = string(y)
-		}
+		s = json2Yaml(json)
 	}
-	return s
+	return
 }
 
-// read json from stdin
-func main() {
-	if fi, err := os.Stdin.Stat(); err != nil {
-		panic(err)
+func json2Yaml(json []byte) (s string) {
+	if y, err := yaml.JSONToYAML(json); err != nil {
+		s = err.Error()
 	} else {
-		if fi.Size() > 0 {
-			fmt.Println(stdin2yaml())
-		} else {
-			fmt.Println("stdin is empty")
-		}
+		s = string(y)
 	}
+	return
 }
