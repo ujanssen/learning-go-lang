2e00b4f7e2b1efdf068c7e5e5771c31490340e9d
Uwe Janssen <mail@uwe-janssen.de>
2015-12-12 10:29:39 +0100

    old worker pools

---
+++ concurrency/pools/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+import "time"
+import "flag"
+import "runtime"
+
+//go run main.go -worker=2 -tasks=20
+func main() {
+	numWorker := flag.Int("worker", runtime.NumCPU(), "num worker")
+	numTasks := flag.Int("tasks", 20, "num tasks")
+	flag.Parse()
+
+	input := make(chan task, *numTasks)
+	output := make(chan task, *numTasks)
+
+	// create tasks
+	for id := 1; id <= *numTasks; id++ {
+		input <- task{id: id, output: output}
+	}
+	close(input)
+
+	// create Worker
+	for w := 1; w <= *numWorker; w++ {
+		go doWork(w, input)
+	}
+
+	// read results
+	for a := 1; a <= *numTasks; a++ {
+		d := <-output
+		fmt.Println("task", d.id, "result", d.result)
+	}
+}
+
+func doWork(id int, input <-chan task) {
+	for t := range input {
+		fmt.Println("worker", id, "processing task", t.id)
+		time.Sleep(time.Second)
+		t.result = t.id * 2
+		t.output <- t
+	}
+}
+
+type task struct {
+	id     int
+	result int
+	output chan task
+}
